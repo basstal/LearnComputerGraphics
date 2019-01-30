f024baf186e1287b852ae5ee73e0b8ff2e96060d
basstal <330475004@qq.com>
2019-01-31 00:28:48 +0800

    变换

--- OpenGL/FragmentShader.glsl
+++ OpenGL/FragmentShader.glsl
@@ -2,8 +2,17 @@
 out vec4 FragColor;
 
 in vec4 vertexColor; // uniform 不需要 in 
+in vec2 textureCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float transparent;
 
 void main()
 {
-	FragColor = vertexColor;
+	// FragColor = texture(texture1, textureCoord / 2);
+	vec2 texture2Coord = vec2(- textureCoord.x, textureCoord.y);
+	FragColor = mix(texture(texture1, textureCoord), texture(texture2, texture2Coord), transparent);
+	// FragColor = texture(texture1, textureCoord / 2) * texture(texture2, textureCoord);
+	// FragColor = texture(texture1, textureCoord);
 }
--- OpenGL/VertexShader.glsl
+++ OpenGL/VertexShader.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec4 vertexColor;
+out vec2 textureCoord;
 
 void main()
 {
 	gl_Position = vec4(aPos, 1.0f);
 	vertexColor = vec4(aColor, 1.0f);
+	textureCoord = aTexCoord;
 }
