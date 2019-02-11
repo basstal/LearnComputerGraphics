cd5538acf46774590f318a5bd70ae49500d03034
王骏科 <wangjunke@wangjunkedeMacBook-Pro.local>
2019-02-11 13:43:18 +0800

    Mesh

---
+++ OpenGL/mesh.cpp
@@ -0,0 +1,101 @@
+#include <string>
+#include <vector>
+
+#include <shader.h>
+
+#include <glm/matrix.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoord;
+};
+
+struct Texture
+{
+    unsigned int ID;
+    string Type;
+};
+
+class Mesh
+{
+public:
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    void Draw(Shader shader);
+private:
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+
+    setupMesh();
+}
+
+void Mesh::setupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+    // indices
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, TexCoord));
+
+    glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader shader)
+{
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+
+    for ( int i = 0 ; i < textures.size(); ++ i)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+
+        string number;
+        string name = textures[i].type;
+        if( name == "texture_diffuse" )
+            number = to_string(diffuseNr ++) ;
+        else if( name = "texture_specular" )
+            number = to_string(specularNr ++) ; 
+        
+        shader.setInt(("material." + name + number), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].ID);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
