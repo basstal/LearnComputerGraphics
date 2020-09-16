1d30eea96d7102149e902cc13db63a358d48ad4c
basstal <basstal@WS.91ACT.COM>
2020-09-16 13:26:08 +0800

    Hello_Triangle update

--- 1_Getting_Started/2_Hello_Triangle/Triangle.cpp
+++ 1_Getting_Started/2_Hello_Triangle/Triangle.cpp
@@ -7,9 +7,20 @@
 OpenGL doesn't simply transform all your 3D coordinates to 2D pixels on your screen; OpenGL only processes 3D coordinates when they're in a specific range between -1.0 and 1.0 on all 3 axes (x, y and z). All coordinates within this so called normalized device coordinates range will end up visible on your screen (and all coordinates outside this region won't).
 */
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    -0.5f, 0.5f, 0.0f
+};
+
+float vertices1[] = {
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f
+};
+
+unsigned int indices[] = {
+    0, 1, 3, 
+    1, 2, 3
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -26,9 +37,18 @@ const char *fragmentShaderSource ="#version 330 core\n"
 " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n";
 
+const char *yellowFragmentShaderSource ="#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+" FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\n";
+const bool wireframe = false;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-void checkShaderCompileStatus(unsigned int shaderId)
+void checkShaderProgramLinkStatus(unsigned int programId);
+void checkShaderCompileStatus(unsigned int shaderId);
 
 int main()
 {
@@ -54,20 +74,6 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    /*
-    The fourth parameter specifies how we want the graphics card to manage the given data. This can take 3 forms:
-
-        GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
-        GL_STATIC_DRAW: the data is set only once and used many times.
-        GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
-    */
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     /* Compile Vertex Shader */
     unsigned int vertexShader;
     vertexShader =glCreateShader(GL_VERTEX_SHADER);
@@ -83,38 +89,119 @@ int main()
     glCompileShader(fragmentShader);
     checkShaderCompileStatus(fragmentShader);
 
+    unsigned int yellowFragmentShader;
+    yellowFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(yellowFragmentShader, 1, &yellowFragmentShaderSource, NULL);
+    glCompileShader(yellowFragmentShader);
+    checkShaderCompileStatus(yellowFragmentShader);
+
     /* Create Shader Program */
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+    checkShaderProgramLinkStatus(shaderProgram);
 
-    int success;
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        char infoLog[512];
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout<< "ERROR::SHADER::LINK_FAILED\n" << infoLog<<std::endl;
-    }
-    glUseProgram(shaderProgram);
-    
+    unsigned int yellowShaderProgram;
+    yellowShaderProgram = glCreateProgram();
+    glAttachShader(yellowShaderProgram, vertexShader);
+    glAttachShader(yellowShaderProgram, yellowFragmentShader);
+    glLinkProgram(yellowShaderProgram);
+    checkShaderProgramLinkStatus(yellowShaderProgram);
+
+    glDeleteShader(yellowFragmentShader);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    /*
+    The fourth parameter specifies how we want the graphics card to manage the given data. This can take 3 forms:
+
+        GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+        GL_STATIC_DRAW: the data is set only once and used many times.
+        GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. then set our vertex attributes pointers
+    /*
+    The function glVertexAttribPointer has quite a few parameters so let's carefully walk through them:
+
+        . The first parameter specifies which vertex attribute we want to configure. Remember that we specified the location of the position vertex attribute in the vertex shader with layout (location = 0). This sets the location of the vertex attribute to 0 and since we want to pass data to this vertex attribute, we pass in 0.
+        . The next argument specifies the size of the vertex attribute. The vertex attribute is a vec3 so it is composed of 3 values.
+        . The third argument specifies the type of the data which is GL_FLOAT (a vec* in GLSL consists of floating point values).
+        . The next argument specifies if we want the data to be normalized. If we're inputting integer data types (int, byte) and we've set this to GL_TRUE, the integer data is normalized to 0 (or -1 for signed data) and 1 when converted to float. This is not relevant for us so we'll leave this at GL_FALSE.
+        . The fifth argument is known as the stride and tells us the space between consecutive vertex attributes. Since the next set of position data is located exactly 3 times the size of a float away we specify that value as the stride. Note that since we know that the array is tightly packed (there is no space between the next vertex attribute value) we could've also specified the stride as 0 to let OpenGL determine the stride (this only works when values are tightly packed). Whenever we have more vertex attributes we have to carefully define the spacing between each vertex attribute but we'll get to see more examples of that later on.
+        . The last parameter is of type void* and thus requires that weird cast. This is the offset of where the position data begins in the buffer. Since the position data is at the start of the data array this value is just 0. We will explore this parameter in more detail later on
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    unsigned int VAO1, VBO1;
+    glGenVertexArrays(1, &VAO1);
+    glGenBuffers(1, &VBO1);
+    glBindVertexArray(VAO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
 
+        if(wireframe)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        // ..:: Drawing code (in render loop) :: ..
+        // 4. draw the object
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // ** if not use EBO
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUseProgram(yellowShaderProgram);
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
     glfwTerminate();
     return 0;
+}
+
+void checkShaderProgramLinkStatus(unsigned int programId)
+{
+    int success;
+    glGetProgramiv(programId, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(programId, 512, NULL, infoLog);
+        std::cout<< "ERROR::SHADER::LINK_FAILED\n" << infoLog<<std::endl;
+    }
 }
 
 void checkShaderCompileStatus(unsigned int shaderId)
@@ -137,5 +224,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
+    }
 }
