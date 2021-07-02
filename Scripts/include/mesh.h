e25680c6fb46eb1087b2444db93139fce2be0caf
basstal <330475004@qq.com>
2021-07-02 18:24:32 +0800

    use cmake to build project

---
+++ Scripts/include/mesh.h
@@ -0,0 +1,133 @@
+#ifndef _MESH_H_
+#define _MESH_H_
+
+#include <assimp/types.h>
+
+#include <string>
+#include <vector>
+
+#include <shader.h>
+
+#include <glm/matrix.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+    glm::vec3 Tangent;
+};
+
+struct Texture
+{
+    unsigned int ID;
+    std::string Type;
+    aiString Path;
+};
+
+class Mesh
+{
+public:
+    // mesh data
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader shader);
+    void BindTexture(Shader shader);
+    unsigned int GetVAO();
+private:
+    // render data
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+
+    setupMesh();
+}
+
+unsigned int Mesh::GetVAO()
+{
+    return VAO;
+}
+
+void Mesh::setupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    /*
+    if we were to represent a struct as an array of data, it would only contain the struct's variables in sequential order which directly translates to a float (actually byte) array that we want for an array buffer.
+    */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+
+    glEnableVertexAttribArray(1);
+    /* 
+    Another great use of structs is a preprocessor directive called offsetof(s,m) that takes as its first argument a struct and as its second argument a variable name of the struct. The macro returns the byte offset of that variable from the start of the struct. 
+    */
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, Normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, TexCoords));
+
+    glBindVertexArray(0);
+}
+
+void Mesh::BindTexture(Shader shader)
+{
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+    unsigned int reflectionNr = 1;
+    unsigned int tangentNr = 1;
+
+    // std::cout << "mesh textures size = " << textures.size() << std::endl;
+    for (unsigned int i = 0 ; i < textures.size(); ++ i)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+
+        std::string number;
+        std::string name = textures[i].Type;
+        if( name == "texture_diffuse" )
+            number = std::to_string(diffuseNr ++) ;
+        else if( name == "texture_specular" )
+            number = std::to_string(specularNr ++) ; 
+        else if (name == "texture_reflection")
+            number = std::to_string(reflectionNr ++ );
+        else if (name == "texture_tangent")
+            number = std::to_string(tangentNr ++ );
+
+        shader.setInt(("material." + name + number), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].ID);
+    }
+}
+void Mesh::Draw(Shader shader)
+{
+    BindTexture(shader);
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+#endif
---
+++ Scripts/include/utils.h
@@ -0,0 +1,142 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <string>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+unsigned int LoadSkyboxTex(std::vector<std::string> skyboxTexs)
+{
+    unsigned int skyboxTex;
+    glGenTextures(1, &skyboxTex);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTex);
+
+    int width, height, nrChannel;
+    for (unsigned int i = 0 ; i < skyboxTexs.size(); ++i)
+    {
+        unsigned char * data = stbi_load(skyboxTexs[i].c_str(), &width, &height, &nrChannel, 0);
+        GLint format;
+        if (nrChannel == 1)
+            format = GL_RED;
+        else if (nrChannel == 3)
+            format = GL_RGB;
+        else if (nrChannel == 4)
+            format = GL_RGBA;
+
+        if ( data )
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        else
+            std::cout << "ERROR::LOAD SKYBOX TEXTURE FALIED" << std::endl;
+        stbi_image_free(data);
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+    return skyboxTex;
+}
+
+
+unsigned int loadImageGamma(const char * path, bool openGammaCorrection, bool flipOnLoad)
+{
+    unsigned int texture;
+    glGenTextures(1, &texture);
+
+    int width, height, nrChannel;
+    stbi_set_flip_vertically_on_load(flipOnLoad);
+    unsigned char * data = stbi_load(path, &width, &height, &nrChannel, 0);
+    if (data != NULL)
+    {
+        GLint format;
+        GLint internalFormat;
+
+        if (nrChannel == 1)
+            internalFormat = format = GL_RED;
+        else if (nrChannel == 3)
+        {
+            format = GL_RGB;
+            internalFormat = openGammaCorrection ? GL_SRGB : GL_RGB;
+        }
+        else if (nrChannel == 4)
+        {
+            internalFormat = openGammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;   
+            format = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "ERROR::LOAD IMAGE FAILED!" << std::endl;
+    }
+    stbi_image_free(data);
+
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    return texture;
+}
+
+unsigned int loadImage(const char * path, const std::string &directory, bool flipOnLoad)
+{
+    std::string fileName = std::string(path);
+    if (!directory.empty())
+        fileName = directory + '/' + fileName;
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+
+    int width, height, nrchannel;
+    stbi_set_flip_vertically_on_load(flipOnLoad);
+    unsigned char * data = stbi_load(fileName.c_str(), &width, &height, &nrchannel, 0);
+    if ( data != NULL )
+    {
+        GLint format;
+        if (nrchannel == 1)
+            format = GL_RED;
+        else if (nrchannel == 3)
+            format = GL_RGB;
+        else if (nrchannel == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "ERROR::LOAD IMAGE::FAILED!" << std::endl;
+    }
+    
+    stbi_image_free(data);
+    return texture;
+}
+
+void replaceStringInPlace(std::string& subject, const std::string& search, const std::string& replace) {
+    if (search.empty())
+    {
+        return;
+    }
+    size_t pos = 0;
+    while((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+}
+
+#endif
